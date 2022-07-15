3b4264e37cd8eb971e7eb1df4773067f41497eeb
Sam Liebert <samliebert@sams-mbp.lan>
2022-07-15 13:18:03 -0500

    added toast

--- pages/enter.jsx
+++ pages/enter.jsx
@@ -1,7 +1,37 @@
+import { auth, googleAuthProvider } from "../lib/firebase";
+
 export default function EnterPage({ }) {
+	const user = null;
+	const username = null;
+	
 	return (
 			<main>
-                <h1>Sign Up</h1>
+                {user ? 
+					!username ? <UsernameForm/> : <SignOutButton/>
+					:
+					<SignInButton/>
+				}
 		    </main>
 	)
 }
+
+function SignInButton() {
+	const signInWithGoogle = async () => {
+		await auth.signInWithPopup(googleAuthProvider);
+	};
+	return(
+		<button className="btn-google" onClick={signInWithGoogle}>
+			<img src={'/google.avif'} alt="" />
+		</button>
+	);
+}
+  
+  // Sign out button
+function SignOutButton() {
+	return <button onClick={() => auth.signOut()}>Sign Out</button>
+}
+  
+function UsernameForm() {
+	
+}
+  
